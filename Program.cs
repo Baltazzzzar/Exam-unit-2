d9a8f054fd485165ddf69f6a07ed5e8254a275f4
Baltazzzzar <dariok@student.uia.no>
2024-02-22 12:29:53 +0100

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+//ID: c40b8fc46fb28dc1a89da405e192fc8b543f709d8b19c13bd6e2ece5d777cb7b
+// start/c40b8fc46fb28dc1a89da405e192fc8b543f709d8b19c13bd6e2ece5d777cb7b
+// {"taskID":"rEu25ZX"}
