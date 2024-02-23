050e1d783bff8c38740f57a702d6daf8866e9e5a
Baltazzzzar <dariok@student.uia.no>
2024-02-23 21:44:06 +0100

    moved Constants to seperate file to improve readability

---
+++ Constants.cs
@@ -0,0 +1,7 @@
+public class Constants
+{
+    public const string myPersonalID = "c40b8fc46fb28dc1a89da405e192fc8b543f709d8b19c13bd6e2ece5d777cb7b";
+    public const string baseURL = "https://mm-203-module-2-server.onrender.com/";
+    public const string startEndpoint = "start/";
+    public const string taskEndpoint = "task/";
+}
--- Program.cs
+++ Program.cs
@@ -8,16 +8,10 @@ using System.Diagnostics.CodeAnalysis;
 Console.Clear();
 Console.WriteLine("Starting Assignment 2");
 
-// SETUP 
-const string myPersonalID = "c40b8fc46fb28dc1a89da405e192fc8b543f709d8b19c13bd6e2ece5d777cb7b";
-const string baseURL = "https://mm-203-module-2-server.onrender.com/";
-const string startEndpoint = "start/";
-const string taskEndpoint = "task/";
-
 HttpUtils httpUtils = HttpUtils.instance;
 
 //#### REGISTRATION
-Response startRespons = await httpUtils.Get(baseURL + startEndpoint + myPersonalID);
+Response startRespons = await httpUtils.Get(Constants.baseURL + Constants.startEndpoint + Constants.myPersonalID);
 Console.WriteLine($"Start:\n{Colors.Magenta}{startRespons}{ANSICodes.Reset}\n\n"); // Print the response from the server to the console
 string taskID = ""; // We get the taskID from the previous response and use it to get the task (look at the console output to find the taskID)
 
@@ -26,14 +20,14 @@ string taskID = ""; // We get the taskID from the previous response and use it t
 //#### FIRST TASK 
 // Gathering the task
 taskID = "rEu25ZX";
-Response task1Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
+Response task1Response = await httpUtils.Get(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID);
 Task task1 = Functions.GetTask(task1Response);
 
 //Solution
 int answerTaskOne = TaskSolver.SolveTaskOne(task1.parameters);
 
 // Sending the answer to the server
-Response task1AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answerTaskOne.ToString());
+Response task1AnswerResponse = await httpUtils.Post(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID, answerTaskOne.ToString());
 Functions.WriteAnswerMessage(task1AnswerResponse);
 
 
@@ -41,14 +35,14 @@ Functions.WriteAnswerMessage(task1AnswerResponse);
 //#### SECOND TASK
 // Gathering the task
 taskID = "otYK2";
-Response task2Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
+Response task2Response = await httpUtils.Get(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID);
 Task task2 = Functions.GetTask(task2Response);
 
 //Solution
 string answerTaskTwo = TaskSolver.SolveTaskTwo(task2.parameters);
 
 // Sending the answer to the server
-Response task2AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answerTaskTwo);
+Response task2AnswerResponse = await httpUtils.Post(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID, answerTaskTwo);
 Functions.WriteAnswerMessage(task2AnswerResponse);
 
 
@@ -56,14 +50,14 @@ Functions.WriteAnswerMessage(task2AnswerResponse);
 //#### THIRD TASK
 // Gathering the task
 taskID = "psu31_4";
-Response task3Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
+Response task3Response = await httpUtils.Get(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID);
 Task task3 = Functions.GetTask(task3Response);
 
 //Solution
 int answerTaskThree = TaskSolver.SolveTaskThree(task3.parameters);
 
 // Sending the answer to the server
-Response task3AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answerTaskThree.ToString());
+Response task3AnswerResponse = await httpUtils.Post(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID, answerTaskThree.ToString());
 Functions.WriteAnswerMessage(task3AnswerResponse);
 
 
@@ -71,14 +65,14 @@ Functions.WriteAnswerMessage(task3AnswerResponse);
 //#### FOURTH TASK
 // Gathering the task
 taskID = "kuTw53L";
-Response task4Response = await httpUtils.Get(baseURL + taskEndpoint + myPersonalID + "/" + taskID);
+Response task4Response = await httpUtils.Get(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID);
 Task task4 = Functions.GetTask(task4Response);
 
 //Solution
 string answerTaskFour = TaskSolver.SolveTaskFour(task4.parameters);
 
 // Sending the answer to the server
-Response task4AnswerResponse = await httpUtils.Post(baseURL + taskEndpoint + myPersonalID + "/" + taskID, answerTaskFour);
+Response task4AnswerResponse = await httpUtils.Post(Constants.baseURL + Constants.taskEndpoint + Constants.myPersonalID + "/" + taskID, answerTaskFour);
 Functions.WriteAnswerMessage(task4AnswerResponse);
 
 public class Task
